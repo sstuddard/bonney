3392b480f345482802450f8c2e845efb4a5d85e2
Shayne Studdard <sstuddard@gmail.com>
2015-09-29 15:20:26 -0500

    Updating project.clj

--- project.clj
+++ project.clj
@@ -1,9 +1,9 @@
-(defproject bonney "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
+(defproject bonney "0.1.0"
+  :description "Bonney: A Clojure scheduling library"
+  :url "https://github.com/sstuddard/bonney"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.7.0"]]
+  :dependencies [[org.clojure/clojure "1.6.0"]]
   :main ^:skip-aot bonney.core
   :target-path "target/%s"
   :profiles {:uberjar {:aot :all}})
