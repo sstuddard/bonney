6bd9b4ae6da520f3ed3b7c736474341216557c50
Shayne Studdard <sstuddard@gmail.com>
2015-09-29 15:37:52 -0500

    Fix repl ns

--- project.clj
+++ project.clj
@@ -4,6 +4,6 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.6.0"]]
-  :main ^:skip-aot bonney.core
+  :repl-options {:init-ns bonney.core }
   :target-path "target/%s"
   :profiles {:uberjar {:aot :all}})
